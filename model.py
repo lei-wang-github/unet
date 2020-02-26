b0be3bd1a8ac02311071b9743831b81b62e40a39
Lei Wang <c6177841383@gmail.com>
2020-02-25 21:36:00 -0500

    reduce model size by reducing its feature maps count (ratio)

--- model.py
+++ model.py
@@ -63,9 +63,11 @@ from keras import backend as keras
 #
 #     return model
 
+img_height = 512
+img_width = 512
 
-def unet(pretrained_weights=None, input_size=(512, 512, 1)):
-    reduction_ratio = 20
+def unet(pretrained_weights=None, input_size=(img_height, img_width, 1)):
+    reduction_ratio = 32
     inputs = Input(input_size)
     conv1 = Conv2D(int(64/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
     conv1 = Conv2D(int(64/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
@@ -80,29 +82,29 @@ def unet(pretrained_weights=None, input_size=(512, 512, 1)):
     conv4 = Conv2D(int(512/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
     drop4 = Dropout(0.5)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-    
+
     conv5 = Conv2D(int(1024/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
     conv5 = Conv2D(int(1024/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
     drop5 = Dropout(0.5)(conv5)
-    
+
     up6 = Conv2D(int(512/reduction_ratio), 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling2D(size=(2, 2))(drop5))
     merge6 = concatenate([drop4, up6], axis=3)
     conv6 = Conv2D(int(512/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
     conv6 = Conv2D(int(512/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
-    
+
     up7 = Conv2D(int(256/reduction_ratio), 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling2D(size=(2, 2))(conv6))
     merge7 = concatenate([conv3, up7], axis=3)
     conv7 = Conv2D(int(256/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
     conv7 = Conv2D(int(256/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
-    
+
     up8 = Conv2D(int(128/reduction_ratio), 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling2D(size=(2, 2))(conv7))
     merge8 = concatenate([conv2, up8], axis=3)
     conv8 = Conv2D(int(128/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
     conv8 = Conv2D(int(128/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
-    
+
     up9 = Conv2D(int(64/reduction_ratio), 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling2D(size=(2, 2))(conv8))
     merge9 = concatenate([conv1, up9], axis=3)
@@ -110,14 +112,14 @@ def unet(pretrained_weights=None, input_size=(512, 512, 1)):
     conv9 = Conv2D(int(64/reduction_ratio), 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
-    
+
     model = Model(input=inputs, output=conv10)
-    
+
     model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
-    
+
     model.summary()
-    
+
     if (pretrained_weights):
         model.load_weights(pretrained_weights)
-    
+
     return model
